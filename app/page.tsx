fe5c6e3f8998bd6fde1b2651d12516f23f0aac07
Afro104 <royjr104@gmail.com>
2024-07-22 11:32:31 -0700

    pdf might work now?

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,38 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import { faFile } from "@fortawesome/free-regular-svg-icons/faFile";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Box, Modal, Typography } from "@mui/material";
+import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
+import "@cyntler/react-doc-viewer/dist/index.css";
+
+interface Document {
+  uri: string;
+}
 
 export default function Home() {
+  const style = {
+    position: 'absolute',
+    top: '29rem',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '35rem',
+    height: '56rem',
+    bgcolor: 'transparent',
+    zIndex : 0
+  };
+
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [docs, setDocs] = useState<Document[]>([]);
+  const resumePDF = '../resume2024.pdf';
+
+  useEffect(() => {
+    const tempDocs: Document[] = [{ uri: resumePDF }];
+    setDocs(tempDocs);
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
       <div className="box z-40">
@@ -102,6 +129,64 @@ export default function Home() {
         <div className="star"></div>
         <div className="star"></div>
       </div>
+      <Modal 
+        className='overflow-x-scroll overflow-x-hidden scrollbar'
+        open={open}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        BackdropProps={{
+          onClick: (event) => event.stopPropagation(), // Prevent modal from closing on backdrop click
+        }}
+      >
+        <Box sx={style}>
+          <div className="wrapper">
+            {[...Array(15)].map((_, index) => (
+              <div key={index}>
+                <span className="dot"></span>
+              </div>
+            ))}
+          </div>
+          <Box
+            sx={{
+              position: 'relative',
+              zIndex: 2,
+              backgroundColor: 'transparent',
+              padding: 2,
+            }}
+          >
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              <div className="shadow">Resume</div>
+            </Typography>
+            <DocViewer
+              documents={docs}
+              initialActiveDocument={docs[1]}
+              pluginRenderers={DocViewerRenderers}
+              theme={{
+                primary: "#5296d8",
+                secondary: "#ffffff",
+                tertiary: "#5296d899",
+                textPrimary: "#ffffff",
+                textSecondary: "#5296d8",
+                textTertiary: "#00000099",
+                disableThemeScrollbar: false,
+              }}
+              config={{
+                header: {
+                  disableHeader: true,
+                  disableFileName: true,
+                },
+                pdfZoom : {
+                  defaultZoom : 1.24,
+                  zoomJump : .2
+                }
+              }}
+            />
+          </Box>
+          <div className='FooterButtons'>
+            <button className='BtnClose' onClick={handleClose}></button>
+          </div>
+        </Box>
+      </Modal>
     </main>
   );
 }
